155682012bda42b22176025f1161756092f318b6
AndriiHorbatiuk <andrey.gorbatyuk@gmail.com>
2016-10-20 14:00:07 +0300

    Deleted unused trash

--- src/main/java/com/horbatiuk/scheduller/schedulerTasks/Schedulable.java
+++
@@ -1,7 +0,0 @@
-package com.horbatiuk.scheduller.schedulerTasks;
-
-/**
- * Created by Andrey on 20.09.2016.
- */
-public interface Schedulable {
-}
--- src/main/java/com/horbatiuk/watchers/watchingData/iWatching.java
+++
@@ -1,7 +0,0 @@
-package com.horbatiuk.watchers.watchingData;
-
-/**
- * Created by Andrey on 16.09.2016.
- */
-public interface iWatching {
-}
